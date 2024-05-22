df1379d6200c9e03c86e24546d7e082a1ef52ed4
kzv-nursultan <kazievnursultan5554@gmail.com>
2024-05-22 15:52:36 +0600

    init file and added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+
+export const HEADER_TITLES: string[] = [
+  "name",
+  "email",
+  "age",
+  "position",
+  "department",
+  "action",
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="border w-full whitespace-nowrap">
+        <thead className="bg-gray-50">
+          <tr>
+            {HEADER_TITLES.map((title, i) => (
+              <th
+                key={title}
+                scope="col"
+                className={`px-6 py-3 capitalize border ${
+                  !i && "sticky left-0 h-fit text-black bg-slate-200"
+                }`}
+              >
+                {title}
+              </th>
+            ))}
+          </tr>
+        </thead>
+      </table>
     </div>
   );
 }
