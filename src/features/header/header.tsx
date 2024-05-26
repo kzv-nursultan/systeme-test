bbb299f65803881d53bd40a82cfe33c32a905abc
kzv-nursultan <kazievnursultan5554@gmail.com>
2024-05-26 15:29:28 +0600

    fixed styles

--- src/features/header/header.tsx
+++ src/features/header/header.tsx
@@ -1,28 +1,29 @@
-import { useMemo } from "react";
 import { NAVIGATION } from "../../app/constants/navigation";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
-  const currentPath = useMemo(() => window.location.pathname, []);
+  const navigate = useNavigate();
+
   return (
     <header className="bg-slate-200 p-3">
       <div
-        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1 p-3"
+        className="justify-between items-center w-full lg:flex lg:w-auto lg:order-1 p-3"
         id="mobile-menu-2"
       >
         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
           {NAVIGATION.map((nav) => (
             <li
               key={nav.href}
-              className="max-lg:border-b border-gray-300 max-lg:py-3 px-3"
+              className="border-gray-300 max-lg:py-3 px-3"
             >
-              <a
-                href={nav.href}
+              <button
+                onClick={() => navigate(nav.href)}
                 className={`hover:text-[#007bff] text-gray-500 block font-semibold text-[15px] active:text-black ${
-                  currentPath === nav.href && "text-orange-500"
+                  window.location.pathname === nav.href && "text-orange-500"
                 }`}
               >
                 {nav.title}
-              </a>
+              </button>
             </li>
           ))}
         </ul>
