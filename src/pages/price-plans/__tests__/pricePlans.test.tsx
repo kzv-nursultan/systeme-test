453f530e9befc7b24b196caac8088a70a11a3fde
kzv-nursultan <kazievnursultan5554@gmail.com>
2024-05-26 09:08:53 +0600

    fixed titles

--- src/pages/price-plans/__tests__/pricePlans.test.tsx
+++ src/pages/price-plans/__tests__/pricePlans.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import PricePlans from "../pricePlans";
 
-describe("Pages page render", () => {
+describe("Price plan page render", () => {
   test("it should match snapshot", () => {
     const view = render(<PricePlans />);
     expect(view).toMatchSnapshot();
--- src/pages/products/__tests__/products.test.tsx
+++ src/pages/products/__tests__/products.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import Products from "../products";
 
-describe("Pages page render", () => {
+describe("Products page render", () => {
   test("it should match snapshot", () => {
     const view = render(<Products />);
     expect(view).toMatchSnapshot();
